e94759d0e039f645d366f56b145bcb65f792913f
Muhammad Amir Khan <amir.khan@gmx.com>
2016-06-14 11:20:49 +0500

    Added server url again due to application error.

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID,
   masterKey: process.env.MASTER_KEY,
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337',
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
