89b6f3ff3e243d5f83026d881c63959f0917d42a
Muhammad Amir Khan <amir.khan@gmx.com>
2016-06-14 09:47:45 +0500

    Replaced hardcoded database URI and keys with environment variables.

--- index.js
+++ index.js
@@ -5,18 +5,11 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = 'mongodb://rockmobile-admin:PkF-5fd-zxE-xrb@ds011374.mlab.com:11374/rockmobile-dev';
-
-if (!databaseUri) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
-}
-
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: process.env.DATABASE_URI || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'Q4b7fVvyYCVQKiZnaU075d1pyGQfl8r91AWXNXyE',
-  masterKey: process.env.MASTER_KEY || 'L7ndNbt5v21g2UTh7JRJuhIhgEBtIOBbWYnR7Okm', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID,
+  masterKey: process.env.MASTER_KEY,
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
